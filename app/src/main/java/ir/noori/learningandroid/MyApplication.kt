efae0ab046fa474437d7bbb8961b781bafb81958
fatemeh <fatimanoori1998@gmail.com>
2024-11-14 11:10:19 +0330

    #7 remove handler in Application

--- app/src/main/java/ir/noori/learningandroid/MyApplication.kt
+++ app/src/main/java/ir/noori/learningandroid/MyApplication.kt
@@ -2,19 +2,16 @@ package ir.noori.learningandroid
 
 import android.app.Application
 import android.content.Context
-import android.os.Handler
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 open class MyApplication : Application() {
     companion object {
         lateinit var context: Context
-        lateinit var handler: Handler
     }
 
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
-        handler = Handler()
     }
 }
