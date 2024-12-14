bd361a97085a9891ee4b750b2d5bfe0474acbc4f
fatemeh <fatimanoori1998@gmail.com>
2024-12-14 16:12:27 +0330

    remove context in myApplication

--- app/src/main/java/ir/noori/learningandroid/MyApplication.kt
+++ app/src/main/java/ir/noori/learningandroid/MyApplication.kt
@@ -1,17 +1,12 @@
 package ir.noori.learningandroid
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 open class MyApplication : Application() {
-    companion object {
-        lateinit var context: Context
-    }
 
     override fun onCreate() {
         super.onCreate()
-        context = applicationContext
     }
 }
